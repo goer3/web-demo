b3dbc36c658ecb70f094a933b931fcfc89569e87
DK <1214966109@qq.com>
2026-06-20 17:23:26 +0800

    简化样式，优化配置

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Outlet, useNavigate, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
 import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
   FundOutlined,
   DesktopOutlined,
   AlertOutlined,
@@ -26,8 +30,6 @@ import {
   CreditCardOutlined,
   QuestionCircleOutlined,
   GithubOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
   MoreOutlined,
   WarningTwoTone,
   CheckOutlined,
@@ -35,276 +37,212 @@ import {
   HeartTwoTone,
   BgColorsOutlined
 } from '@ant-design/icons';
-import { Layout, Menu, Button, Avatar, Dropdown, Badge } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
-import { LogoWhite, LogoBlack, LogoCollapsed, DefaultAvatar } from '@/components/image';
-import { SiderDivider } from '@/components/common';
-import { GenerateGenderBadge } from '@/components/badge';
-import { CURRENT_VERSION } from '@/config';
+import { Button, Layout, Menu, ConfigProvider } from 'antd';
+import { LogoWhite, LogoBlack, LogoCollapsed } from '@/components/image';
 
-// 修改主题
-let customTheme = localStorage.getItem('theme') || 'light';
-const switchThemeHandler = () => {
-  customTheme = customTheme === 'light' ? 'dark' : 'light';
-  localStorage.setItem('theme', customTheme);
-  location.reload();
-};
+const { Header, Sider, Content, Footer } = Layout;
 
-const item1 = [
+// 自定义主题
+let customTheme = localStorage.getItem('customTheme') || 'dark';
+customTheme = 'light';
+
+// 菜单配置
+const menuItems = [
   {
-    key: '/dashboard',
-    label: '工作空间',
-    icon: <DesktopOutlined />
-  },
-  {
-    key: '/query',
-    label: '数据查询',
-    icon: <FundOutlined />
-  },
-  {
-    key: '/datasource',
-    label: '数据来源',
-    icon: <HddOutlined />
-  },
-  {
-    key: '/alarm',
-    label: '监控告警',
-    icon: <AlertOutlined />,
-    popupOffset: [20, 0],
+    key: 'group1',
+    label: '系统功能',
+    type: 'group',
     children: [
-      { key: '/alarm/events', label: '告警事件', icon: <ExceptionOutlined /> },
-      { key: '/alarm/rules', label: '告警策略', icon: <BorderlessTableOutlined /> },
-      { key: '/alarm/shielding', label: '屏蔽策略', icon: <AudioMutedOutlined /> },
       {
-        key: '/alarm/history',
-        label: '告警历史',
-        icon: <FieldTimeOutlined />,
+        key: '/dashboard',
+        label: '工作空间',
+        icon: <DesktopOutlined />
+      },
+      {
+        key: '/query',
+        label: '数据查询',
+        icon: <FundOutlined />
+      },
+      {
+        key: '/datasource',
+        label: '数据来源',
+        icon: <HddOutlined />
+      },
+      {
+        key: '/alarm',
+        label: '监控告警',
+        icon: <AlertOutlined />,
         children: [
-          { key: '/alarm/history/todo', label: '等待处理', icon: <WarningTwoTone twoToneColor="#eb2f96" /> },
-          { key: '/alarm/history/finish', label: '完成处理', icon: <CheckOutlined /> }
+          { key: '/alarm/events', label: '告警事件', icon: <ExceptionOutlined /> },
+          { key: '/alarm/rules', label: '告警策略', icon: <BorderlessTableOutlined /> },
+          { key: '/alarm/shielding', label: '屏蔽策略', icon: <AudioMutedOutlined /> },
+          {
+            key: '/alarm/history',
+            label: '告警历史',
+            icon: <FieldTimeOutlined />,
+            children: [
+              { key: '/alarm/history/todo', label: '等待处理', icon: <WarningTwoTone twoToneColor="#eb2f96" /> },
+              { key: '/alarm/history/finish', label: '完成处理', icon: <CheckOutlined /> }
+            ]
+          }
         ]
+      },
+      {
+        key: '/message',
+        label: '消息通知',
+        icon: <MailOutlined />,
+        children: [
+          { key: '/message/type', label: '告警媒介', icon: <AppstoreAddOutlined /> },
+          { key: '/message/template', label: '通知模板', icon: <SnippetsOutlined /> }
+        ]
+      },
+      {
+        key: '/user',
+        label: '人员组织',
+        icon: <UsergroupAddOutlined />,
+        children: [
+          { key: '/user/list', label: '用户列表', icon: <SolutionOutlined /> },
+          { key: '/user/groups', label: '用户分组', icon: <CommentOutlined /> },
+          { key: '/user/projects', label: '项目团队', icon: <ClusterOutlined /> },
+          { key: '/user/scheduling', label: '人员排班', icon: <CalendarOutlined /> }
+        ]
+      },
+      {
+        key: '/system',
+        label: '系统设置',
+        icon: <SlidersOutlined />,
+        children: [
+          { key: '/system/role', label: '角色授权', icon: <IdcardOutlined /> },
+          { key: '/system/menu', label: '菜单配置', icon: <SisternodeOutlined /> },
+          { key: '/system/api', label: '接口配置', icon: <ApiOutlined /> },
+          { key: '/system/setting', label: '通用设置', icon: <SettingOutlined /> }
+        ]
+      },
+      {
+        key: '/log',
+        label: '日志审计',
+        icon: <AuditOutlined />
       }
     ]
   },
   {
-    key: '/message',
-    label: '消息通知',
-    icon: <MailOutlined />,
-    popupOffset: [20, 0],
+    key: 'group2',
+    label: '附加功能',
+    type: 'group',
     children: [
-      { key: '/message/type', label: '告警媒介', icon: <AppstoreAddOutlined /> },
-      { key: '/message/template', label: '通知模板', icon: <SnippetsOutlined /> }
+      {
+        key: '/profile',
+        label: '个人中心',
+        icon: <CreditCardOutlined />
+      },
+      {
+        key: '/template',
+        label: '开发模板',
+        icon: <HeartTwoTone />,
+        children: [
+          { key: '/template/button', label: '按钮' },
+          { key: '/template/form', label: '表单' },
+          { key: '/template/table', label: '表格' }
+        ]
+      },
+      {
+        key: '/help',
+        label: '获取帮助',
+        icon: <QuestionCircleOutlined />
+      },
+      {
+        key: '/update',
+        label: '版本更新',
+        icon: <GithubOutlined />
+      }
     ]
-  },
-  {
-    key: '/user',
-    label: '人员组织',
-    icon: <UsergroupAddOutlined />,
-    popupOffset: [20, 0],
-    children: [
-      { key: '/user/list', label: '用户列表', icon: <SolutionOutlined /> },
-      { key: '/user/groups', label: '用户分组', icon: <CommentOutlined /> },
-      { key: '/user/projects', label: '项目团队', icon: <ClusterOutlined /> },
-      { key: '/user/scheduling', label: '人员排班', icon: <CalendarOutlined /> }
-    ]
-  },
-  {
-    key: '/system',
-    label: '系统设置',
-    icon: <SlidersOutlined />,
-    popupOffset: [20, 0],
-    children: [
-      { key: '/system/role', label: '角色授权', icon: <IdcardOutlined /> },
-      { key: '/system/menu', label: '菜单配置', icon: <SisternodeOutlined /> },
-      { key: '/system/api', label: '接口配置', icon: <ApiOutlined /> },
-      { key: '/system/setting', label: '通用设置', icon: <SettingOutlined /> }
-    ]
-  },
-  {
-    key: '/log',
-    label: '日志审计',
-    icon: <AuditOutlined />
   }
 ];
 
-const item2 = [
-  {
-    key: '/profile',
-    label: '个人中心',
-    icon: <CreditCardOutlined />
+const siderThemeColor = {
+  light: {
+    primaryColor: '#000000',
+    siderBg: '#f7f8f9',
+    menu: {}
   },
-  {
-    key: '/template',
-    label: '开发模板',
-    icon: <HeartTwoTone />,
-    popupOffset: [20, 0],
-    children: [
-      { key: '/template/button', label: '按钮' },
-      { key: '/template/form', label: '表单' },
-      { key: '/template/table', label: '表格' }
-    ]
-  },
-  {
-    key: '/help',
-    label: '获取帮助',
-    icon: <QuestionCircleOutlined />
-  },
-  {
-    key: '/update',
-    label: '版本更新',
-    icon: <GithubOutlined />
+  dark: {
+    primaryColor: '#000000',
+    siderBg: '#000000',
+    menu: {}
   }
-];
-
-const dropdownItems = [
-  {
-    key: '1',
-    label: (
-      <div className="s-dropdown-user">
-        <Badge size="small" className="s-badge-gender" count={GenerateGenderBadge(1)} offset={[-5, 23]}>
-          <Avatar className="s-dropdown-user__avatar" size={30} src={DefaultAvatar} />
-        </Badge>
-        <div className="s-dropdown-user__info">
-          <div className="s-dropdown-user__name">吴彦祖</div>
-          <div className="s-dropdown-user__desc">互联网不知名换皮工程师</div>
-        </div>
-      </div>
-    )
-  },
-  {
-    type: 'divider'
-  },
-  {
-    key: 'profile',
-    label: '个人中心',
-    icon: <CreditCardOutlined />
-  },
-  {
-    key: 'switchTheme',
-    label: '切换主题',
-    icon: <BgColorsOutlined />,
-    onClick: switchThemeHandler
-  },
-  {
-    type: 'divider'
-  },
-  {
-    key: 'logout',
-    label: '注销登录',
-    danger: true,
-    icon: <LogoutOutlined />
-  }
-];
-
-const AdminLayout = () => {
-  // 菜单展开折叠
-  const [collapsed, setCollapsed] = useState(false);
-  // 路由跳转
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-
-  // 版本提示
-  const [showVersionTip, setShowVersionTip] = useState(false);
-
-  useEffect(() => {
-    const savedVersion = localStorage.getItem('version');
-    if (savedVersion !== CURRENT_VERSION) {
-      setShowVersionTip(true);
-    }
-  }, []);
-
-  const dismissVersionTip = () => {
-    localStorage.setItem('version', CURRENT_VERSION);
-    setShowVersionTip(false);
-  };
-
-  // 点击菜单跳转
-  const clickMenuItemHandler = React.useCallback(({ key }) => navigate(key), [navigate]);
-
-  return (
-    <>
-      <Layout hasSider>
-        <Sider
-          className="s-sider"
-          width={220}
-          collapsedWidth={60}
-          breakpoint="lg"
-          onBreakpoint={broken => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-          trigger={null}
-          collapsible
-          collapsed={collapsed}
-        >
-          {/* logo */}
-          <div className="s-logo">
-            <img src={collapsed ? LogoCollapsed : customTheme === 'light' ? LogoBlack : LogoWhite} alt="logo" />
-          </div>
-          {/* 菜单 */}
-          <div className="s-body">
-            {collapsed ? null : <div className="s-group">平台功能</div>}
-            <Menu
-              theme="light"
-              mode="inline"
-              inlineIndent={15}
-              defaultSelectedKeys={['alarm']}
-              items={item1}
-              selectedKeys={[pathname]}
-              onClick={clickMenuItemHandler}
-            />
-            {collapsed ? <SiderDivider /> : <div className="s-group">更多功能</div>}
-            <Menu
-              theme="light"
-              mode="inline"
-              inlineIndent={15}
-              defaultSelectedKeys={['4']}
-              items={item2}
-              selectedKeys={[pathname]}
-              onClick={clickMenuItemHandler}
-            />
-          </div>
-          {/* 用户 */}
-          <div className="s-trigger">
-            {collapsed ? (
-              <Dropdown className="s-trigger__menu" menu={{ items: dropdownItems }} placement="rightBottom">
-                <Badge size="small" className="s-badge-gender" count={GenerateGenderBadge(1)} offset={[-5, 23]}>
-                  <Avatar className="s-trigger__avatar" size={30} src={DefaultAvatar} style={{ cursor: 'pointer' }} />
-                </Badge>
-              </Dropdown>
-            ) : (
-              <>
-                <Badge size="small" className="s-badge-gender" count={GenerateGenderBadge(1)} offset={[-5, 23]}>
-                  <Avatar className="s-trigger__avatar" size={30} src={DefaultAvatar} />
-                </Badge>
-                <div className="s-trigger__info">
-                  <div className="s-trigger__name">吴彦祖</div>
-                  <div className="s-trigger__desc">互联网换皮工程师</div>
-                </div>
-                <Dropdown className="s-trigger__menu" menu={{ items: dropdownItems }} placement="rightBottom">
-                  <MoreOutlined style={{ cursor: 'pointer' }} />
-                </Dropdown>
-              </>
-            )}
-          </div>
-        </Sider>
-        <Button
-          className="s-collapsed"
-          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          onClick={() => setCollapsed(!collapsed)}
-        ></Button>
-        <Layout>
-          <Header></Header>
-          <Content>
-            <Outlet />
-          </Content>
-          <Footer style={{ textAlign: 'center', lineHeight: '50px' }}>Ant Design ©2026 Created by Ant UED</Footer>
-        </Layout>
-      </Layout>
-    </>
-  );
 };
 
+// Sider 单独主题定制，便于切换主题
+const siderThemeConfig = {
+  zeroRuntime: true,
+  token: {},
+  components: {
+    Layout: {
+      siderBg: siderThemeColor[customTheme].siderBg
+    },
+    Menu: {
+      // 通用
+      marginXS: 5,
+      margin: 15,
+      paddingXS: 5,
+      padding: 15,
+      itemHeight: 30,
+      boxShadowSecondary: 'none',
+      collapsedIconSize: 16,
+      dropdownWidth: 100,
+      subMenuItemSelectedColor: '#000000',
+      groupTitleLineHeight: '30px',
+      itemActiveBg: 'transparent',
+      // light 主题
+      itemBg: '#f7f8f9',
+      itemColor: '#000000',
+      subMenuItemBg: '#f7f8f9',
+      itemSelectedBg: '#f7f8f9',
+      itemSelectedColor: '#0052d9',
+      popupBg: '#f7f8f9',
+      // dark 主题
+      darkItemBg: '#000000',
+      darkItemColor: '#ffffff',
+      darkSubMenuItemBg: '#000000',
+      darkItemSelectedBg: '#000000',
+      darkItemSelectedColor: '#ff0000',
+      darkPopupBg: '#000000'
+    }
+  }
+};
+
+// 特殊样式
+const logoStyle = {
+  backgroundColor: siderThemeColor[customTheme].siderBg
+};
+
+const AdminLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <Layout>
+      <ConfigProvider theme={siderThemeConfig}>
+        <Sider className="s-sider" width={220} collapsedWidth={60} theme={customTheme} trigger={null} collapsible collapsed={collapsed}>
+          <div className="s-logo" style={logoStyle}>
+            <img src={collapsed ? LogoCollapsed : customTheme === 'dark' ? LogoWhite : LogoBlack} alt="logo" />
+          </div>
+          <Menu
+            className="s-menu"
+            theme={customTheme}
+            openKeys={['/alarm', '/alarm/history']}
+            inlineIndent={25}
+            mode="inline"
+            items={menuItems}
+          />
+        </Sider>
+      </ConfigProvider>
+      <Layout>
+        <Header className="s-header">
+          <Button type="text" icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />} onClick={() => setCollapsed(!collapsed)} />
+        </Header>
+        <Content className="s-content">Content</Content>
+        <Footer className="s-footer">Footer</Footer>
+      </Layout>
+    </Layout>
+  );
+};
 export default AdminLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,7 @@ dayjs.locale('zh-cn');
 
 // 样式
 import 'antd/dist/antd.css';
-import '@/assets/css/light.less';
-
-// 默认主题
-let customTheme = localStorage.getItem('theme') || 'light';
-// 根据不同主题引入不同的样式
-if (customTheme === 'dark') {
-  import('@/assets/css/dark.less');
-}
+import '@/assets/css/theme.less';
 
 // 字体
 import 'misans/lib/Normal/MiSans-Regular.min.css';
@@ -25,128 +18,35 @@ import 'misans/lib/Normal/MiSans-Regular.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { GenerateRoutes } from '@/router/rules';
 
-// 颜色
-const customColorBlue = '#0052d9';
-const customColorLight = '#ffffff';
-const customColorDark = '#000000';
-const customColorLightGray = '#f4f4f4';
-const customColorFillGray = '#e5e5e5';
-const customColorGray = '#f7f8f9';
-
-// 通用样式
-const customControlHeight = 28;
-const customTableLineHeight = 32;
-const customPaddingInline = 10;
-const customMainBg = '#ffffff';
-
-// light 样式
-let customSiderBg = customColorGray;
-let customSiderColor = customColorDark;
-let customMenuItemSelectedColor = customColorDark;
-let customMenuItemSelectedBg = customColorFillGray;
-
-// dark 样式
-if (customTheme === 'dark') {
-  customSiderBg = customColorDark;
-  customSiderColor = customColorLight;
-  customMenuItemSelectedColor = '#ff0000';
-  customMenuItemSelectedBg = customSiderBg;
-}
-
 // 主题定制
 const themeConfig = {
   zeroRuntime: true,
   token: {
-    colorPrimary: customColorDark,
-    colorPrimaryHover: customColorDark,
-    colorLinkHover: customColorDark,
-    colorLink: customColorBlue,
-    controlItemBgActive: customColorLightGray,
-    controlItemBgHover: customColorLightGray,
-    lineHeight: '20px',
     fontFamily: 'MiSans, serif',
     fontSize: 13,
-    controlInteractiveSize: 13,
-    borderRadius: 0
+    borderRadius: 0,
+    margin: 10,
+    marginSM: 8,
+    marginXS: 5,
+    marginMD: 15,
+    marginLG: 20,
+    paddingXS: 5,
+    paddingSM: 8,
+    padding: 10,
+    paddingMD: 15,
+    paddingLG: 20,
+    marginXL: 30,
+    paddingXL: 30,
+    marginXXL: 40
   },
   components: {
     Layout: {
-      headerBg: customMainBg,
-      headerPadding: 0,
-      bodyBg: customMainBg,
-      footerBg: customMainBg,
+      headerBg: '#ffffff',
+      bodyBg: '#ffffff',
+      footerBg: '#ffffff',
       footerPadding: 0,
-      headerHeight: 50,
-      siderBg: customSiderBg
-    },
-    Menu: {
-      iconSize: 13,
-      collapsedIconSize: 16,
-      itemColor: customSiderColor,
-      itemHeight: 30,
-      itemBg: customSiderBg,
-      subMenuItemBg: customSiderBg,
-      subMenuItemSelectedColor: customSiderColor,
-      itemMarginInline: 20,
-      itemPaddingInline: 0,
-      itemMarginBlock: 4,
-      itemActiveBg: customSiderBg,
-      itemSelectedBg: customMenuItemSelectedBg,
-      itemSelectedColor: customMenuItemSelectedColor,
-      itemHoverColor: customSiderColor,
-      dropdownWidth: 'auto'
-    },
-    Button: {
-      contentFontSize: 12,
-      contentFontSizeSM: 12,
-      defaultShadow: 'none',
-      primaryShadow: 'none',
-      dangerShadow: 'none',
-      controlHeight: customControlHeight,
-      controlHeightSM: 24,
-      paddingInline: customPaddingInline,
-      paddingInlineSM: customPaddingInline,
-      blue6: customColorBlue
-    },
-    Form: {
-      labelHeight: customControlHeight
-    },
-    Input: {
-      activeShadow: 'none',
-      errorActiveShadow: 'none',
-      warningActiveShadow: 'none',
-      controlHeight: customControlHeight,
-      activeBorderColor: customColorFillGray,
-      paddingBlockSM: 4
-    },
-    InputNumber: {
-      controlHeight: customControlHeight,
-      activeBorderColor: customColorFillGray
-    },
-    Slider: {
-      railSize: 8,
-      handleSize: 8,
-      handleLineWidth: 1
-    },
-    Select: {
-      optionHeight: customControlHeight,
-      optionLineHeight: customControlHeight + 'px',
-      optionPadding: '0 10px',
-      optionSelectedBg: customColorFillGray
-    },
-    Table: {
-      lineHeight: customTableLineHeight + 'px',
-      cellPaddingInline: customPaddingInline,
-      cellPaddingBlock: 0
-    },
-    Pagination: {
-      itemSize: 20,
-      itemActiveBg: 'transparent',
-      itemActiveColor: 'red',
-      itemActiveColorHover: 'red'
-    },
-    Dropdown: {
-      paddingBlock: 5
+      headerHeight: 60,
+      headerPadding: '0 20px'
     }
   }
 };
